3a9f71353c89281a94e26ffb48246e043eebd7ad
Malin Persson <malin.persson02@hotmail.com>
2025-10-30 11:17:41 +0100

    Fixed require links on index.php page

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <?php
-require __DIR__ . "header.php";
+require_once __DIR__ . '/header.php';
 ?>
-    <h6>PORTFOLIO</h6>
-    <h1>Dolly Parton</h1>
-    <p>Islands in the stream. That is what we are. No one in between How can we be wrong Sail away with me. To another world. And we rely on each other, huhn hah</p>
-    <section class="container">
-        <div>
-            <img src="images/31955827992ce311098d950fe595deeee9d33791.jpg" alt="image_1">
-        </div>
-        <div>
-            <img src="images/6c85d0d65cbc48a189f5b2d02b6996cfc74db23b.jpg" alt="image_2">
-        </div>
-        <div>
-            <img src="images/821d996c3e3baf530416611af4f42472c0412194.jpg" alt="image_3">
-        </div>
-        <div>
-            <img src="images/9217e70e16d83b6f4341c988df7d8b515abf4b79.jpg" alt="image_4">
-        </div>
-        <div>
-            <img src="images/fb1da43c7c183467ac489032fbac771603d5b48c.jpg" alt="image_5">
-        </div>
-        <div>
-            <img src="" alt="">
-        </div>
-    </section>
+<h6>PORTFOLIO</h6>
+<h1>Dolly Parton</h1>
+<p>Islands in the stream. That is what we are. No one in between How can we be wrong Sail away with me. To another world. And we rely on each other, huhn hah</p>
+<section class="container">
+    <div>
+        <img src="images/31955827992ce311098d950fe595deeee9d33791.jpg" alt="image_1">
+    </div>
+    <div>
+        <img src="images/6c85d0d65cbc48a189f5b2d02b6996cfc74db23b.jpg" alt="image_2">
+    </div>
+    <div>
+        <img src="images/821d996c3e3baf530416611af4f42472c0412194.jpg" alt="image_3">
+    </div>
+    <div>
+        <img src="images/9217e70e16d83b6f4341c988df7d8b515abf4b79.jpg" alt="image_4">
+    </div>
+    <div>
+        <img src="images/fb1da43c7c183467ac489032fbac771603d5b48c.jpg" alt="image_5">
+    </div>
+    <div>
+        <img src="" alt="">
+    </div>
+</section>
 </body>
+
 </html>
